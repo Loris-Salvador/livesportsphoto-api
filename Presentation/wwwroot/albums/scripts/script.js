9f42ff98000253b8d895e8cfa7a1134ae0c56dbe
Loris Salvador <dev.loris.salvador@gmail.com>
2024-10-21 21:19:28 +0200

    js detached

---
+++ Presentation/wwwroot/albums/scripts/script.js
@@ -0,0 +1,11 @@
+﻿const albums = document.querySelectorAll('.album');
+
+albums.forEach(album => {
+    album.addEventListener('click', function() {
+        const link = album.getAttribute('data-link');
+        if (link) {
+            window.location.href = link;
+        }
+    });
+});
+
